70fdfa3fd74690fac0c1f2105b027894d619291c
andreichiro <andreichiro@gmail.com>
2023-11-21 18:34:43 -0300

    Testing airflow dag with all tables from Fideliza

--- dags/emr_dag2.py
+++ dags/emr_dag2.py
@@ -33,16 +33,6 @@ def setup_sql_to_s3_tasks(table_names):
         s3_key = f'raw/{table_name}.sql'
         s3_path = f's3://{Variable.get("s3_bucket")}/{s3_key}'
         s3_paths.append(s3_path)
-
-        upload_to_s3_task = SqlToS3Operator(
-            task_id=f'mysql_to_s3_{table_name}',
-            sql_conn_id="sql_rewards",
-            aws_conn_id="aws_conn_id",
-            query=f"SELECT * FROM {table_name}",
-            s3_bucket=Variable.get("s3_bucket"),
-            s3_key=s3_key,
-            replace=True
-        )
     return s3_paths
 
 # Task to trigger the EMR Serverless Spark job
@@ -90,10 +80,22 @@ description='DAG to transfer data from MySQL to S3 and trigger an EMR Serverless
 )
 
 def sql_to_s3_to_emr_serverless_dag():
-    table_names_task = get_table_names()
+    table_names = get_table_names()
    
-    s3_paths = setup_sql_to_s3_tasks(table_names_task)
-   
+    s3_paths = setup_sql_to_s3_tasks(table_names)
+ 
+    for table_name in table_names:
+        upload_to_s3 = SqlToS3Operator(
+            task_id=f'upload_{table_name}_to_s3',
+            sql_conn_id='sql_rewards',
+            aws_conn_id='aws_conn_id',
+            query=f'SELECT * FROM {table_name}',
+            s3_bucket=Variable.get('s3_bucket'),
+            s3_key=f'raw/{table_name}.sql',
+            replace=True
+        )
+        s3_paths >> upload_to_s3
+
     trigger_emr_instance = trigger_emr_serverless_spark_job(s3_paths)
    
     emr_serverless_sensor_instance = emr_serverless_sensor(trigger_emr_instance)
