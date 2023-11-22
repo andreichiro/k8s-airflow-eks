f0b0b1b334aca6aea79440ce0ee5a5ad87a25c29
andreichiro <andreichiro@gmail.com>
2023-11-22 16:34:35 -0300

    Testing Spark on k8s

--- dags/emr_dag2.py
+++ dags/emr_dag2.py
@@ -8,10 +8,13 @@ from airflow.decorators.sensor import sensor_task
 from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.operators.emr_create_job_flow import EmrCreateJobFlowOperator
+
+
 from airflow.providers.amazon.aws.sensors.emr_job_flow import EmrJobFlowSensor
 from airflow.providers.amazon.aws.operators.emr_add_steps import EmrAddStepsOperator
 from airflow.providers.amazon.aws.sensors.emr_step import EmrStepSensor
 from airflow.providers.amazon.aws.operators.emr_create_notebook_execution import EmrCreateNotebookExecutionOperator
+
 
 # Default arguments for the DAG
 default_args = {
