e664d7e27df7749968bed0686949132469faf615
andreichiro <andreichiro@gmail.com>
2023-11-23 17:42:50 -0300

    Polars + parallel processing + dynamic tasks

--- dags/emr_dag2.py
+++ dags/emr_dag2.py
@@ -38,27 +38,19 @@ def generate_s3_keys(table_names):
     files_paths = [f'raw/{table_name}.parquet' for table_name in table_names]
     return files_paths
 
-@task
-def process_and_upload_to_s3(table_name, s3_key):
-    """
-    Task to process data and upload it to S3.
-    """
-    s3_bucket = Variable.get("s3_bucket")
-    mysql_hook = MySqlHook(mysql_conn_id='sql_rewards')
-    sql = f"SELECT * FROM `{table_name}`"
-    pandas_df = mysql_hook.get_pandas_df(sql)
-    polars_df = pl.from_pandas(pandas_df)
-    parquet_data = polars_df.to_parquet()
 
-    s3_hook = S3Hook(aws_conn_id='aws_conn_id')
-    
-    # Upload the Parquet data to S3
-    s3_hook.load_bytes(
-        parquet_data,
-        key=f'{s3_bucket}/{s3_key}',
-        bucket_name=s3_bucket,
-        replace=True
+@task
+def create_sql_to_s3_tasks(table_names, s3_keys, sql_conn_id, s3_bucket):
+    for table_name, s3_key in zip(table_names, s3_keys):
+        SqlToS3Operator(
+            task_id=f"sql_to_s3_{table_name}",
+            sql_conn_id=sql_conn_id,
+            query=f"SELECT * FROM `{table_name}`",
+            s3_bucket=s3_bucket,
+            s3_key=s3_key,
+            replace=True
         )
+
 # Define the main DAG
 with DAG(
     'my_dynamic_dag',
@@ -74,20 +66,6 @@ with DAG(
     # Task 2: Generate S3 keys
     s3_keys_task = generate_s3_keys(table_names_task)
 
-
-    @task_group(group_id='sql_to_s3_group')
-    def process_tables_to_s3(tables, s3_keys):
-        for table, s3_key in zip(tables, s3_keys):
-            sql_to_s3_task = SqlToS3Operator(
-                task_id=f"sql_to_s3_{table}",
-                sql_conn_id='your_actual_sql_connection_id',  # Replace with your actual connection ID
-                query=f"SELECT * FROM `{table}`",
-                s3_bucket=Variable.get("s3_bucket"),
-                s3_key=s3_key,
-                replace=True
-            )
-            
-    # Instantiate the task group and set up dependencies
-   # Instantiate the task group and set up dependencies
-    process_group = process_tables_to_s3(table_names_task, s3_keys_task)
-    table_names_task >> s3_keys_task >> process_group
+    create_sql_to_s3_tasks(table_names_task, s3_keys_task, 'sql_rewards', Variable.get("s3_bucket"))
+  
+    #sql_to_s3_tasks = [create_sql_to_s3_task(table, s3_key) for table, s3_key in zip(table_names_task, s3_keys_task)]
