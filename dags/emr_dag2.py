9b375daf591384042e15e0f48f9b2bab97714602
andreichiro <andreichiro@gmail.com>
2023-11-24 15:38:40 -0300

    Polars + parallel processing

--- dags/emr_dag2.py
+++ dags/emr_dag2.py
@@ -31,6 +31,38 @@ def get_table_names():
     table_names = [table[0] for table in tables]  # Adjust based on the structure of the returned data
     return table_names
 
+
+def generate_s3_tasks(table_names, **kwargs):
+    for table_name in table_names:
+        SqlToS3Operator(
+            task_id=f"sql_to_s3_{table_name}",
+            sql_conn_id='sql_rewards',
+            query=f"SELECT * FROM `{table_name}`",
+            s3_bucket=Variable.get("s3_bucket"),
+            s3_key=f'raw/{table_name}.parquet',
+            replace=True,
+            dag=kwargs['dag']
+        )
+
+with DAG('my_dynamic_dag', default_args=default_args, schedule_interval=None, catchup=False, tags=['example']) as dag:
+    
+    get_tables = PythonOperator(
+        task_id='get_table_names',
+        python_callable=get_table_names,
+        provide_context=True,
+        dag=dag
+    )
+
+    generate_s3 = PythonOperator(
+        task_id='generate_s3_tasks',
+        python_callable=generate_s3_tasks,
+        provide_context=True,
+        op_kwargs={'dag': dag},
+        dag=dag
+    )
+
+    get_tables >> generate_s3
+
 #@task
 #def generate_s3_keys(table_names):
 #    """
@@ -39,24 +71,24 @@ def get_table_names():
 #    files_paths = [f'raw/{table_name}.parquet' for table_name in table_names]
 #    return files_paths###
 
-@task
-def create_sql_to_s3_task(table_name):
-    """
-    Task to create and execute SqlToS3Operator for a specific table.
-    """
-    mysql_hook = MySqlHook(mysql_conn_id='sql_rewards')
-    sql = f"SELECT * FROM `{table_name}`"
-    s3_key = f'raw/{table_name}.parquet'
-    s3_bucket = Variable.get("s3_bucket")
+#@task
+#def create_sql_to_s3_task(table_name):
+#    """
+#    Task to create and execute SqlToS3Operator for a specific table.
+#    """
+#    mysql_hook = MySqlHook(mysql_conn_id='sql_rewards')
+#    sql = f"SELECT * FROM `{table_name}`"
+#    s3_key = f'raw/{table_name}.parquet'
+#    s3_bucket = Variable.get("s3_bucket")
 
-    sql_operator = SqlToS3Operator(
-        task_id=f"sql_to_s3_{table_name}",
-        sql_conn_id='sql_rewards',
-        query=sql,
-        s3_bucket=s3_bucket,
-        s3_key=s3_key,
-        replace=True
-    )
+#    sql_operator = SqlToS3Operator(
+#        task_id=f"sql_to_s3_{table_name}",
+#        sql_conn_id='sql_rewards',
+#        query=sql,
+#        s3_bucket=s3_bucket,
+#        s3_key=s3_key,
+#        replace=True
+#    )
     
 #@task
 #def create_sql_to_s3_task(table_name):
