49855a084bd7f8326c2134cfc33cf08cda478d37
andreichiro <andreichiro@gmail.com>
2023-11-24 16:07:00 -0300

    Polars + parallel processing

--- dags/emr_dag2.py
+++ dags/emr_dag2.py
@@ -40,25 +40,32 @@ def get_table_names():
 #    return files_paths###
 
 @task
-def create_sql_to_s3_task(table_name):
+def create_sql_to_s3_task(table_name, mysql_conn_id='sql_rewards', s3_bucket=Variable.get("s3_bucket")):
     """
     Task to create and execute SqlToS3Operator for a specific table.
     """
     mysql_hook = MySqlHook(mysql_conn_id='sql_rewards')
-    sql = f"SELECT * FROM `{table_name}`"
-    s3_key = f'raw/{table_name}.parquet'
-    s3_bucket = Variable.get("s3_bucket")
+    df = mysql_hook.get_pandas_df(f"SELECT * FROM `{table_name}`")
+    parquet_buffer = df.to_parquet(index=False)
 
-    sql_operator = SqlToS3Operator(
-        task_id=f"sql_to_s3_{table_name}",
-        sql_conn_id='sql_rewards',
-        query=sql,
-        s3_bucket=s3_bucket,
-        s3_key=s3_key,
-        replace=True,
-        provide_context=True,
-        op_kwargs={'dag': dag},
-    )
+    s3_hook = S3Hook(aws_conn_id='aws_default')
+    s3_key = f'raw/{table_name}.parquet'
+    s3_hook.load_bytes(parquet_buffer, bucket_name=s3_bucket, key=s3_key, replace=True)
+
+    #sql = f"SELECT * FROM `{table_name}`"
+    #s3_key = f'raw/{table_name}.parquet'
+    #s3_bucket = Variable.get("s3_bucket")
+
+#    sql_operator = SqlToS3Operator(
+#        task_id=f"sql_to_s3_{table_name}",
+#        sql_conn_id='sql_rewards',
+#        query=sql,
+#        s3_bucket=s3_bucket,
+#        s3_key=s3_key,
+#        replace=True,
+#        provide_context=True,
+#        op_kwargs={'dag': dag},
+ #   )
     
 #@task
 #def create_sql_to_s3_task(table_name):
@@ -105,4 +112,4 @@ with DAG(
 #        sql_to_s3_tasks.append(sql_to_s3_task)
         
     # Set up dependencies
-#tables >> create_sql_to_s3_tasks
+tables >> create_sql_to_s3_tasks
