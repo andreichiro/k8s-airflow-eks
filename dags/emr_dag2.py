53e0b763c2e56e50e5deb190050c54911172ab0b
andreichiro <andreichiro@gmail.com>
2023-11-27 16:06:51 -0300

    Testing Dags performance

--- dags/emr_dag2.py
+++ dags/emr_dag2.py
@@ -24,75 +24,30 @@ default_args = {
 
 @task
 def get_table_names():
-    """
-    Task to retrieve table names from MySQL database.
-    """
     mysql_hook = MySqlHook(mysql_conn_id='sql_rewards')
     connection = mysql_hook.get_conn()
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM `{table}`")
-    tables = [table[0] for table in cursor.fetchall()]    
-    for table in tables:
-            df = pd.DataFrame(cursor.fetchall())
-            sql_to_s3_task = SqlToS3Operator(
-            task_id=f"sql_to_s3_{table}",
-            sql_conn_id='sql_rewards',
-            query=f"SELECT * FROM `{table}`",
-            s3_bucket=Variable.get("s3_bucket"),
-            s3_key=f'raw/{table}.parquet',
-            replace=True,
-            file_format='parquet',
-            aws_conn_id='aws_conn_id'  # Or your specific AWS connection ID
-        )
+    cursor.execute("SHOW TABLES")
+    return [table[0] for table in cursor.fetchall()]
 
-    
-    #cursor.execute("SHOW TABLES")
-    #tables = [table[0] for table in cursor.fetchall()]
-
-
-@task
-def query_to_s3(table_name):
-        
-    mysql_hook = MySqlHook(mysql_conn_id='sql_rewards')
-    df = mysql_hook.get_pandas_df(f"SELECT * FROM `{table_name}`")
-    connection = mysql_hook.get_conn()
-    cursor = connection.cursor()
-    tables = [table[0] for table in cursor.fetchall()]    
-    
-    
-        
-#@task
-#def generate_s3_keys(table_names):
-#    """
-#    Task to generate S3 keys for storing Parquet files.
-#    """
-#    files_paths = [f'raw/{table_name}.parquet' for table_name in table_names]
-#    return files_paths###
-
-
-#@task
-#def create_sql_to_s3_task(table_name):
-#    """
-#    Task to create and execute SqlToS3Operator for a specific table.
-#    """
-#    return SqlToS3Operator(
-#        task_id=f"sql_to_s3_{table_name}",
-#        sql_conn_id='sql_rewards',  # Replace with your actual connection ID
-#        query=f"SELECT * FROM `{table_name}`",
-#        s3_bucket=Variable.get("s3_bucket"),
-#        s3_key=f'raw/{table_name}.parquet',
-#        replace=True
-#    )
-
-# Define the main DAG
 with DAG(
-    'my_dynamic_dag',
+    dag_id='sql_to_s3_dag',
     default_args=default_args,
-    schedule_interval=None,  # You can set the interval as needed
+    schedule_interval=None,  # Adjust as needed
     catchup=False,
     tags=['example'],
 ) as dag:
     tables = get_table_names()
-    query_to_s3 = query_to_s3.expand(table_name=tables)
+    for table in tables:
+        sql_to_s3_task = SqlToS3Operator(
+            task_id=f'sql_to_s3_{table}',
+            sql_conn_id='sql_rewards',
+            query=f'SELECT * FROM `{table}`',
+            s3_bucket=Variable.get('s3_bucket'),
+            s3_key=f'raw/{table}.parquet',
+            replace=True,
+            file_format='parquet',
+            aws_conn_id='aws_conn_id'
+        )
 
-tables >> query_to_s3
+dag =  get_table_names()
