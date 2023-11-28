938a0a3289d5498654a3f9c904ca7a326c73ea34
andreichiro <andreichiro@gmail.com>
2023-11-28 16:01:08 -0300

    Testing parallel processing

--- dags/emr_dag2.py
+++ dags/emr_dag2.py
@@ -34,29 +34,6 @@ def get_tables():
     cursor.execute('SHOW TABLES;')
     tables = [table[0] for table in cursor.fetchall()]     
     return tables
-#    mysql_hook = MySqlHook(mysql_conn_id='sql_rewards')
-#    connection = mysql_hook.get_conn()
-#    cursor = connection.cursor()
-#    tables = cursor.fetchall()
-#    
-#    for table in tables:
-#            df = pd.read_sql(f"SELECT * FROM `{table}`", connection)
-#            print(df)
-#            sql_to_s3_task = SqlToS3Operator(
-#            task_id=f"sql_to_s3_{table}",
-#            sql_conn_id='sql_rewards',
-#            query=f"SELECT * FROM `{table}`",
-#            s3_bucket=Variable.get("s3_bucket"),
-#            s3_key=f'raw/{table}.parquet',
-#            replace=True,
-#            file_format='parquet',
-#            aws_conn_id='aws_conn_id'  # Or your specific AWS connection ID
-#        )
-
-    
-    #cursor.execute("SHOW TABLES")
-    #tables = [table[0] for table in cursor.fetchall()]
-
 
 def sql_to_s3(table):
     """
@@ -74,25 +51,6 @@ def sql_to_s3(table):
                 aws_conn_id='aws_conn_id'  # Or your specific AWS connection ID
         )
      
-#    files_paths = [f'raw/{table_name}.parquet' for table_name in table_names]
-#    return files_paths###
-
-
-#@task
-#def create_sql_to_s3_task(table_name):
-#    """
-#    Task to create and execute SqlToS3Operator for a specific table.
-#    """
-#    return SqlToS3Operator(
-#        task_id=f"sql_to_s3_{table_name}",
-#        sql_conn_id='sql_rewards',  # Replace with your actual connection ID
-#        query=f"SELECT * FROM `{table_name}`",
-#        s3_bucket=Variable.get("s3_bucket"),
-#        s3_key=f'raw/{table_name}.parquet',
-#        replace=True
-#    )
-
-# Define the main DAG
 with DAG(
     'my_dynamic_dag',
     default_args=default_args,
